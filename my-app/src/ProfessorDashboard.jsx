f804db0877caba03c8190a41b1b68e52abd3930e
ahmedsebai94-blip <ahmedsebai94@gmail.com>
2026-05-09 14:06:01 +0200

    update

--- my-app/src/ProfessorDashboard.jsx
+++ my-app/src/ProfessorDashboard.jsx
@@ -3866,6 +3866,10 @@ const handleSendChatMessage = async () => {
                                             value={aiRiskThreshold}
                                             onChange={(e) => setAiRiskThreshold(Number(e.target.value))}
                                         >
+                                            <option value={10}>10+</option>
+                                            <option value={20}>20+</option>
+                                            <option value={30}>30+</option>
+                                            <option value={40}>40+</option>
                                             <option value={50}>50+</option>
                                             <option value={60}>60+</option>
                                             <option value={70}>70+</option>
